c2c1460495a727cc93793b296901dd489acc1547
roxelmendoza <roxel.mendoza@xurpas.com>
2022-12-21 16:41:53 +0800

    updated ID

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import ReactGA from "react-ga4";
-ReactGA.initialize("G-KF7V1HWCB0");
 function App() {
   useEffect(() => {
+    ReactGA.initialize("G-FVMPNM7JY3");
     ReactGA.send("pageview");
     // ReactGA.send({ hitType: "pageview", page: "/" });
 
     // ReactGA.pageview(window.location.pathname + window.location.search);
-    
   }, []);
 
   const event = (category: string, action: string, label: string) => {
@@ -22,7 +21,9 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => event("Users", "REGISTER", "TEST")}>Register</button>
+      <button onClick={() => event("Users", "REGISTER", "TEST")}>
+        Register
+      </button>
       <button onClick={() => event("Users", "LOGIN", "TEST")}>Login</button>
       <button onClick={() => event("Users", "CLICK", "TEST")}>Click</button>
     </div>
