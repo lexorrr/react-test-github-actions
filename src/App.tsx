a26ee3e29257cf4a39fe4ffc94bb3ce0dcfd9d40
roxelmendoza <roxel.mendoza@xurpas.com>
2022-12-21 16:21:00 +0800

    reverted to GA

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ReactGA from "react-ga4";
 
 function App() {
   useEffect(() => {
-    ReactGA.initialize("UA-KF7V1HWCB0");
+    ReactGA.initialize("GA-KF7V1HWCB0");
 
     ReactGA.send("pageview");
     // ReactGA.send({ hitType: "pageview", page: "/" });
