ff9110d10a33ec1ee94b51fcb7c5b673e2bf6584
roxelmendoza <roxel.mendoza@xurpas.com>
2022-12-22 01:35:36 +0800

    added analytics sheet script

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import ReactGA from "react-ga4";
+
+const ANALYTICS_SHEET_URL =
+  "https://script.google.com/macros/s/AKfycbxGVYKYJKxeZWhgj7IlgxpbBBOK6DgvO2hLkvX0NkR2qORhyVzURW2hhT4d0bteaDvb/exec";
+
 function App() {
   useEffect(() => {
     ReactGA.initialize("G-FVMPNM7JY3", {
@@ -13,6 +17,16 @@ function App() {
     // ReactGA.send({ hitType: "pageview", page: "/" });
 
     // ReactGA.pageview(window.location.pathname + window.location.search);
+
+    const onLoadEvent = async () => {
+      await fetch(
+        `${ANALYTICS_SHEET_URL}?Category=Test&Action=page_visit&Label=visit`,
+        {
+          method: "GET",
+        }
+      );
+    };
+    onLoadEvent();
   }, []);
 
   const event = (category: string, action: string, label: string) => {
