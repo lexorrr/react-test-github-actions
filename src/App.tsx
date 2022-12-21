e33ce277feb65d033f5d5e17ce24cbd140e827b6
roxelmendoza <roxel.mendoza@xurpas.com>
2022-12-21 16:52:01 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,20 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import ReactGA from "react-ga4";
 function App() {
-  useEffect(() => {
-    ReactGA.initialize("G-FVMPNM7JY3");
-    ReactGA.send("pageview");
-    // ReactGA.send({ hitType: "pageview", page: "/" });
+  // useEffect(() => {
+  //   ReactGA.initialize("G-FVMPNM7JY3");
+  //   ReactGA.send("pageview");
+  //   // ReactGA.send({ hitType: "pageview", page: "/" });
 
-    // ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+  //   // ReactGA.pageview(window.location.pathname + window.location.search);
+  // }, []);
 
   const event = (category: string, action: string, label: string) => {
-    ReactGA.event({
-      category,
-      action,
-      label,
-    });
+    // ReactGA.event({
+    //   category,
+    //   action,
+    //   label,
+    // });
   };
 
   return (
